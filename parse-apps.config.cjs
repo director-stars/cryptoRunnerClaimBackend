fac58f900b1075bbf3e7c5cf3b37ada74c4a028b
director-stars <director_9414@outlook.com>
2022-03-30 02:44:31 +0800

    test

---
+++ parse-apps.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+      name        : "mad-wallet-stake",
+      script      : "yarn start",
+      watch       : false,
+      merge_logs  : true,
+      cwd         : "."
+
+     }]
+  }
